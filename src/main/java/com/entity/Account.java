ebabfcc18a9643dec7b19cc656cafb4ef768cbe3
Jovin2014 <jovin.ng.2014@sis.smu.edu.sg>
2019-07-27 11:36:09 +0800

    changing entity

--- src/main/java/com/entity/Account.java
+++ src/main/java/com/entity/Account.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.entity;
 
 /**
@@ -10,18 +6,6 @@ package com.entity;
  * @author jovin ng
  */
 public class Account {
-
-  UserID
-  Password (hashed)
-  FirstName
-  LastName
-  Email
-  Gender
-  BirthDate
-  Nationality
-  Interest
-  Region
-
 
     private String accountid;
     private String password;
