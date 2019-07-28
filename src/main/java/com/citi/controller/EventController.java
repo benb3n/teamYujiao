f1ed3027e338743f17de6b62fa728594d8d08715
Chia Chek Wei <chekwei@me.com>
2019-07-28 16:10:50 +0800

    new codes

--- src/main/java/com/citi/controller/EventController.java
+++ src/main/java/com/citi/controller/EventController.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,8 +18,12 @@ public class EventController {
 	
 	@RequestMapping("/event/view/{id}")
 	@ResponseBody
-    Map<String, String> viewEvent(@PathVariable("id") int eventID) {
-		Map<String, String> result = new HashMap<>();
+	
+	ArrayList<HashMap<String, String>> viewEventRegistrationByID(@PathVariable("id") int eventID) {
+		  ArrayList<HashMap<String, String>> result = new ArrayList<>();
+	
+    //Map<String, String> viewEvent(@PathVariable("id") int eventID) {
+		//Map<String, String> result = new HashMap<>();
 		
 		 System.out.println("starting");
 	         try {
@@ -32,7 +37,10 @@ public class EventController {
 	             System.out.println("Pulled data");
 	             
 	             while (rs.next()) {
-	            	 result.put("OrganiserID", rs.getString(1));
+	            	 HashMap<String, String> rsJSON = new HashMap<>();
+	            	 rsJSON.put("OrganiserID", rs.getString(1));
+	            	 
+	            	 result.add(rsJSON);
 	            	 
 	                 //return "Read from database : " + rs.getString(1) + "  " + rs.getString(2) ;
 	             }
@@ -48,8 +56,11 @@ public class EventController {
 	
 	@RequestMapping("/event/view/")
 	@ResponseBody
-    Map<String, String> viewAllEvents() {
-		Map<String, String> result = new HashMap<>();
+	ArrayList<HashMap<String, String>> viewAllEvents() {
+		  ArrayList<HashMap<String, String>> result = new ArrayList<>();
+	
+    //Map<String, String> viewAllEvents() {
+		//Map<String, String> result = new HashMap<>();
 		
 		 System.out.println("starting");
 	         try {
@@ -63,15 +74,65 @@ public class EventController {
 	             System.out.println("Pulled data");
 	             
 	             while (rs.next()) {
-	            	 result.put("EventID", rs.getString(0));
-	            	 result.put("OrganiserID", rs.getString(1));
-	            	 result.put("EventName", rs.getString(2));
-	            	 result.put("EventDescription", rs.getString(3));
-	            	 result.put("StartTime", rs.getString(4));
+	            	 
+	            	 HashMap<String, String> rsJSON = new HashMap<>();
+	            	 
+	            	 rsJSON.put("EventID", rs.getString(1));
+	            	 //rsJSON.put("OrganiserID", rs.getString(2));
+	            	 rsJSON.put("EventName", rs.getString(3));
+	            	 rsJSON.put("EventDescription", rs.getString(4));
+	            	 //rsJSON.put("StartTime", rs.getString(5));
+	            	 //rsJSON.put("EndTime", rs.getString(6));
+	            	 rsJSON.put("MinPax", ""+rs.getInt(1));
+	            	 rsJSON.put("MaxPax", ""+rs.getInt(2));
+	            	 rsJSON.put("SignupCount", ""+rs.getInt(3));
+	            	 rsJSON.put("Status", rs.getString(7));
+	            	 
+	            	 result.add(rsJSON);
+	            	 
+	                 //return "Read from database : " + rs.getString(1) + "  " + rs.getString(2) ;
+	             }
+	             con.close();
+	         } catch (Exception e) {
+	             System.out.println(e);
+	         }
+	    System.out.println(result.size());  
+	    System.out.println("hi1");  
+		return result;
+//        return "Well Done!! Nice Try!! Good bye!";
+    }
+	
+	
+	@RequestMapping("/event/view/date/{Dates}")
+	@ResponseBody
+    Map<String, String> viewAllEventsFromDates(@PathVariable("Dates")String Dates) {
+		Map<String, String> result = new HashMap<>();
+		
+		 System.out.println("starting");
+	         try {
+	             Class.forName("com.mysql.jdbc.Driver");
+	             String username ="CitiAdmin";
+	             String password = "citihack2019";
+	             Connection con = DriverManager.getConnection("jdbc:mysql://citihack2019.cwop36kfff9j.ap-southeast-1.rds.amazonaws.com:3306/innodb", username, password);
+	 
+	             Statement stmt = con.createStatement();
+	             
+	             String fromDate = Dates.split(":")[0];
+	             String toDate = Dates.split(":")[1];
+	             
+	             ResultSet rs = stmt.executeQuery("select * from Events where StartTime > " + "'" + fromDate + "' AND EndTime < '" + toDate + "'");
+	             System.out.println("Pulled data");
+	             
+	             while (rs.next()) {
+	            	 result.put("EventID", rs.getString(1));
+	            	 result.put("OrganiserID", rs.getString(2));
+	            	 result.put("EventName", rs.getString(3));
+	            	 result.put("EventDescription", rs.getString(4));
+	            	 result.put("StartTime", rs.getString(5));
 	            	 result.put("EndTime", rs.getString(5));
-	            	 result.put("MinPax", ""+rs.getInt(0));
-	            	 result.put("MaxPax", ""+rs.getInt(1));
-	            	 result.put("SignupCount", ""+rs.getInt(2));
+	            	 result.put("MinPax", ""+rs.getInt(1));
+	            	 result.put("MaxPax", ""+rs.getInt(2));
+	            	 result.put("SignupCount", ""+rs.getInt(3));
 	            	 result.put("Status", rs.getString(6));
 	            	 
 	                 //return "Read from database : " + rs.getString(1) + "  " + rs.getString(2) ;
@@ -84,6 +145,89 @@ public class EventController {
 		return result;
 //        return "Well Done!! Nice Try!! Good bye!";
     }
+	
+	
+	@RequestMapping("/event/getEvents/search/{eventName}")
+	@ResponseBody
+    Map<String, String> viewAllEventsByName(@PathVariable("eventName")String eventName) {
+		Map<String, String> result = new HashMap<>();
+		
+		 System.out.println("starting");
+	         try {
+	             Class.forName("com.mysql.jdbc.Driver");
+	             String username ="CitiAdmin";
+	             String password = "citihack2019";
+	             Connection con = DriverManager.getConnection("jdbc:mysql://citihack2019.cwop36kfff9j.ap-southeast-1.rds.amazonaws.com:3306/innodb", username, password);
+	 
+	             Statement stmt = con.createStatement();
+	             ResultSet rs = stmt.executeQuery("select * from Events where eventName like '%" + eventName + "%'");
+	             System.out.println("Pulled data");
+	             
+	             while (rs.next()) {
+	            	 result.put("EventID", rs.getString(1));
+	            	 result.put("OrganiserID", rs.getString(2));
+	            	 result.put("EventName", rs.getString(3));
+	            	 result.put("EventDescription", rs.getString(4));
+	            	 result.put("StartTime", rs.getString(5));
+	            	 result.put("EndTime", rs.getString(5));
+	            	 result.put("MinPax", ""+rs.getInt(1));
+	            	 result.put("MaxPax", ""+rs.getInt(2));
+	            	 result.put("SignupCount", ""+rs.getInt(3));
+	            	 result.put("Status", rs.getString(6));
+	            	 
+	                 //return "Read from database : " + rs.getString(1) + "  " + rs.getString(2) ;
+	             }
+	             con.close();
+	         } catch (Exception e) {
+	             System.out.println(e);
+	         }
+	        
+		return result;
+//        return "Well Done!! Nice Try!! Good bye!";
+    }
+	
+	
+	@RequestMapping("/event/getEvents")
+	@ResponseBody
+    Map<String, String> viewAllEventsFromToday() {
+		Map<String, String> result = new HashMap<>();
+		
+		 System.out.println("starting");
+	         try {
+	             Class.forName("com.mysql.jdbc.Driver");
+	             String username ="CitiAdmin";
+	             String password = "citihack2019";
+	             Connection con = DriverManager.getConnection("jdbc:mysql://citihack2019.cwop36kfff9j.ap-southeast-1.rds.amazonaws.com:3306/innodb", username, password);
+	 
+	             Statement stmt = con.createStatement();
+	             
+	             
+	             ResultSet rs = stmt.executeQuery("select * from Events where StartTime > now()");
+	             System.out.println("Pulled data");
+	             
+	             while (rs.next()) {
+	            	 result.put("EventID", rs.getString(1));
+	            	 result.put("OrganiserID", rs.getString(2));
+	            	 result.put("EventName", rs.getString(3));
+	            	 result.put("EventDescription", rs.getString(4));
+	            	 result.put("StartTime", rs.getString(5));
+	            	 result.put("EndTime", rs.getString(5));
+	            	 result.put("MinPax", ""+rs.getInt(1));
+	            	 result.put("MaxPax", ""+rs.getInt(2));
+	            	 result.put("SignupCount", ""+rs.getInt(3));
+	            	 result.put("Status", rs.getString(6));
+	            	 
+	                 //return "Read from database : " + rs.getString(1) + "  " + rs.getString(2) ;
+	             }
+	             con.close();
+	         } catch (Exception e) {
+	             System.out.println(e);
+	         }
+	        
+		return result;
+//        return "Well Done!! Nice Try!! Good bye!";
+    }
+	
 	
 	
 
--- src/main/java/com/citi/controller/EventRegistrations.java
+++ src/main/java/com/citi/controller/EventRegistrations.java
@@ -1,54 +1,119 @@
 package com.citi.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.*;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EventRegistrations {
 
-	public EventRegistrations(String eventID, String userID, Date signupTime, String status, String feedback) {
-		super();
-		this.eventID = eventID;
-		this.userID = userID;
-		this.signupTime = signupTime;
-		this.status = status;
-		this.feedback = feedback;
-	}
 	
-	private String eventID;
-	private String userID;
-	private Date signupTime;
-	private String status;
-	private String feedback;
+	@RequestMapping("/event/register/{details}")
+	@ResponseBody
+    Map<String, String> insertEvent(@PathVariable("details")String details) {
+		Map<String, String> result = new HashMap<>();
+		
+		 System.out.println("starting");
+	         try {
+	             Class.forName("com.mysql.jdbc.Driver");
+	             String username ="CitiAdmin";
+	             String password = "citihack2019";
+	             Connection con = DriverManager.getConnection("jdbc:mysql://citihack2019.cwop36kfff9j.ap-southeast-1.rds.amazonaws.com:3306/innodb", username, password);
+	 
+	             Statement stmt = con.createStatement();
+	             
+	             String eventID = details.split(":")[0];
+	             String userID = details.split(":")[1];
+	             
+	             stmt.executeUpdate("insert into EventRegistrations (EventID, UserID, Signuptime, Status) Values (" + eventID + "," + userID + ",now(),'pending')");
+	             System.out.println("Inserted data");
+	             
+	            
+	             con.close();
+	         } catch (Exception e) {
+	             System.out.println(e);
+	         }
+	        
+		return result;
+//        return "Well Done!! Nice Try!! Good bye!";
+    }
 	
-	public String getEventID() {
-		return eventID;
-	}
-	public void setEventID(String eventID) {
-		this.eventID = eventID;
-	}
-	public String getUserID() {
-		return userID;
-	}
-	public void setUserID(String userID) {
-		this.userID = userID;
-	}
-	public Date getSignupTime() {
-		return signupTime;
-	}
-	public void setSignupTime(Date signupTime) {
-		this.signupTime = signupTime;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public String getFeedback() {
-		return feedback;
-	}
-	public void setFeedback(String feedback) {
-		this.feedback = feedback;
-	}
+	
+	@RequestMapping("/event/withdraw/{details}")
+	@ResponseBody
+    Map<String, String> withdrawEvent(@PathVariable("details")String details) {
+		Map<String, String> result = new HashMap<>();
+		
+		 System.out.println("starting");
+	         try {
+	             Class.forName("com.mysql.jdbc.Driver");
+	             String username ="CitiAdmin";
+	             String password = "citihack2019";
+	             Connection con = DriverManager.getConnection("jdbc:mysql://citihack2019.cwop36kfff9j.ap-southeast-1.rds.amazonaws.com:3306/innodb", username, password);
+	 
+	             Statement stmt = con.createStatement();
+	             
+	             String eventID = details.split(":")[0];
+	             String userID = details.split(":")[1];
+	          
+	             
+	             stmt.executeUpdate("update EventRegistrations SET Status = 'withdrawn' where EventID = '" + eventID + "' AND UserID = '" + userID + "'");
+	             System.out.println("Updated data");
+	             
+	            
+	             con.close();
+	         } catch (Exception e) {
+	             System.out.println(e);
+	         }
+	        
+		return result;
+//        return "Well Done!! Nice Try!! Good bye!";
+    }
+	
+	
+	@RequestMapping("/event/getEventRegistrations/{id}")
+	 @ResponseBody
+	  ArrayList<HashMap<String, String>> viewEventRegistrationByID(@PathVariable("id") int userID) {
+	  ArrayList<HashMap<String, String>> result = new ArrayList<>();
+	  
+	   System.out.println("starting");
+	          try {
+	              Class.forName("com.mysql.jdbc.Driver");
+	              String username ="CitiAdmin";
+	              String password = "citihack2019";
+	              Connection con = DriverManager.getConnection("jdbc:mysql://citihack2019.cwop36kfff9j.ap-southeast-1.rds.amazonaws.com:3306/innodb", username, password);
+	  
+	              Statement stmt = con.createStatement();
+	              ResultSet rs = stmt.executeQuery("select * from EventRegistrations where UserID = " + userID + " and Status in ('Pending', 'Registered')");
+	              System.out.println("Pulled data");
+	              
+	              while (rs.next()) {
+	               HashMap<String, String> rsJSON = new HashMap<>();
+	               rsJSON.put("EventID", rs.getString(1));
+
+	               rsJSON.put("UserID", rs.getString(2));
+	               rsJSON.put("Signuptime", rs.getString(3));
+	               rsJSON.put("Status", rs.getString(4));
+	               
+	               result.add(rsJSON);
+	                  //return "Read from database : " + rs.getString(1) + "  " + rs.getString(2) ;
+	              }
+	              con.close();
+	          } catch (Exception e) {
+	              System.out.println(e);
+	          }
+	         
+	  return result;
+//	        return "Well Done!! Nice Try!! Good bye!";
+	    }
+	
 	
 	
 }
